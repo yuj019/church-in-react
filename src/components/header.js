7163d19ab7e8ac515bf800bd2d3739abb453d01a
yumeijin <yumeijin0601@gmail.com>
2020-03-02 17:28:33 -0800

    second commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div className="Header">
+  <div className="HeaderGroup">
+    <Link to="/home">Home</Link>
+    <Link to="/announcements">Announcements</Link>
+    <Link to="/readings">Readings</Link>
+    <Link to="/church-services">Church Services</Link>
+    <Link to="/media-library">Media Library</Link>
+    <Link to="/contact-us">Contact Us</Link>
+    <Link to="/donate"><button>Donate</button></Link>
+  </div>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,17 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to the Church in San Jose!</h1>
-    <p>The Lord loves us before we love Him.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    
+    <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Welcome to<br/>Church in San Jose</h1>
+          <p>The Lord loves us before we love Him.</p>
+          <Link to="/video">Join us</Link>
+          <Link to="/page-2/">Go to page 2</Link>
+        </div>
+    </div>
   </div>
+  
 )
 
 export default IndexPage
