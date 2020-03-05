66b4fa6983e663f37a51949b77fd034a2092cbc6
yumeijin <yumeijin0601@gmail.com>
2020-03-04 16:15:38 -0800

    third commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,44 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-  <div className="HeaderGroup">
-    <Link to="/home">Home</Link>
-    <Link to="/announcements">Announcements</Link>
-    <Link to="/readings">Readings</Link>
-    <Link to="/church-services">Church Services</Link>
-    <Link to="/media-library">Media Library</Link>
-    <Link to="/contact-us">Contact Us</Link>
-    <Link to="/donate"><button>Donate</button></Link>
-  </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+}
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/home">Home</Link>
+          <Link to="/announcements">Announcements</Link>
+          <Link to="/readings">Readings</Link>
+          <Link to="/church-services">Church Services</Link>
+          <Link to="/media-library">Media Library</Link>
+          <Link to="/contact-us">Contact Us</Link>
+          <Link to="/donate"><button>Donate</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
